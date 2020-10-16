810e93f155bcb6e982725c941859ce7b9db22f8d
Colin Watterson <ctwatterson123@gmail.com>
2020-10-16 15:35:30 -0700

    Colin Added Names to Files

--- Assets/Scripts/creatureScripts/creatureAttackMelee.cs
+++ Assets/Scripts/creatureScripts/creatureAttackMelee.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//Author : Colin
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "Creature Melee Attack", menuName = "ScriptableObjects/CreatureAttacks/MeleeAttack", order = 2)]
--- Assets/Scripts/creatureScripts/creatureAttackRanged.cs
+++ Assets/Scripts/creatureScripts/creatureAttackRanged.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//Author : Colin
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "Creature Ranged Attack", menuName = "ScriptableObjects/CreatureAttacks/RangedAttack", order = 1)]
