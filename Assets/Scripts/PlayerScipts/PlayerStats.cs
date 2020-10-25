9fdaf10e02435f925687331f2c2d28f70ce8f22b
Jameson Danning <jdanning@ucsc.edu>
2020-10-24 23:27:30 -0700

    health properties

---
+++ Assets/Scripts/PlayerScipts/PlayerStats.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    [SerializeField] private int maxHealth = 500;
+
+    //This is a property. Use MaxHealth when accessing 
+    public int MaxHealth
+    {
+        get
+        {
+            return maxHealth;
+        }
+        set
+        {
+            maxHealth = value;
+            healthUIUpdate();
+        }
+    }
+
+    [SerializeField] private int currHealth;
+    //This is a property. Use CurrHealth when accessing 
+    public int CurrHealth
+    {
+        get
+        {
+            return currHealth;
+        }
+        set
+        {
+            currHealth = value;
+            healthUIUpdate();
+        }
+    }
+
+    public float damage = 10;
+    public float defense = 10;
+    public float speed;
+    public Slider slider;
+    public TextMeshProUGUI maxHealthUI;
+    public TextMeshProUGUI currHealthUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = maxHealth;
+    }
+
+    // updates health UI 
+    void Update()
+    {
+        
+    }
+
+    void healthUIUpdate()
+    {
+        slider.value = (currHealth / maxHealth) * 100;
+        currHealthUI.SetText((Mathf.Round(currHealth)).ToString());
+        maxHealthUI.SetText("/ " + maxHealth.ToString());
+    }
+}
