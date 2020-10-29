5cc6ad9739de1a645a0559738fa2382d8ce1fe52
Jameson Danning <jdanning@ucsc.edu>
2020-10-28 22:02:03 -0700

    enemy health

--- Assets/Scripts/creatureScripts/CreatureStats.cs
+++ Assets/Scripts/creatureScripts/CreatureStats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class PlayerStats : MonoBehaviour
+public class CreatureStats : MonoBehaviour
 {
 
     [SerializeField] private float maxHealth = 500;
