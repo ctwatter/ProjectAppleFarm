db5be6876e7e1f11d83f3d3a56bcffb4925eb089
Jacob Compton <jrcompto@ucsc.edu>
2020-11-02 14:39:57 -0800

    Player can be hit and killed

---
+++ Assets/Scripts/enemyScripts/tempEnemyWeaponTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempEnemyWeaponTrigger : MonoBehaviour
+{
+
+    [SerializeField] private EnemyStats enemyStats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerStats>().takeDamage(enemyStats.damage);
+        }
+    }
+}
