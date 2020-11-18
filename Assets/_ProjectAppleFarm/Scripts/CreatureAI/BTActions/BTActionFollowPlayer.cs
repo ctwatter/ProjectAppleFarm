834160154131da82bd47d32f92f057331c02a69a
Colin Watterson <ctwatterson123@gmail.com>
2020-11-18 11:57:49 -0800

    started work on basic action

---
+++ Assets/_ProjectAppleFarm/Scripts/CreatureAI/BTActions/BTActionFollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTActionFollowPlayer : BTnode
+{
+    public BTActionFollowPlayer(string _name, Transform creature,  Transform target) : base(_name) {
+        name = _name;
+    }
+
+
+
+    public override NodeState Evaluate()
+    {
+
+        return NodeState.RUNNING;
+    }
+}
