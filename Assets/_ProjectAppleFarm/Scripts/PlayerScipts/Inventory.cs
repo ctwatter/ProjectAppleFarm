d591f9ccfdaaa6c809aa688ac5b6c2bdccdc1afc
Eugene Shin <shin.eugene97@gmail.com>
2020-11-24 15:36:39 -0800

    Player Inv Progress
    some groundwork in player inventory script.

--- Assets/_ProjectAppleFarm/Scripts/PlayerScipts/Inventory.cs
+++ Assets/_ProjectAppleFarm/Scripts/PlayerScipts/Inventory.cs
@@ -7,27 +7,41 @@ using System.Collections.Generic;
 [System.Serializable] 
 public class Inventory : ScriptableObject
 {
+    public int index;
     private void Start() {
         {
             //manually add items to inventory in beginninng?
         }
     }
 
-    public List<InventorySlot> inventory = new List<InventorySlot>();
-    public void AddItem (Item _item)
+    public ItemDatabase inventory = new ItemDatabase();
+
+    void AddItem (Item item) 
     {
-        inventory.Add(new InventorySlot(_item));
+        inventory.itemList.Add (item);
+        index = inventory.itemList.Count;
+    }
+    
+    void DeleteItem (int index) 
+    {
+        inventory.itemList.RemoveAt (index);
+    }
+    void DeleteItem (string itemName) 
+    {
+        inventory.itemList.RemoveAll(p => p.itemName == itemName);
     }
 
 }
 
-[System.Serializable] 
-public class InventorySlot
-{
-    public Item item;
-    public InventorySlot(Item _item) {
-        {
-            item = _item;
-        }
-    }
-}
+
+
+// [System.Serializable] 
+// public class InventorySlot
+// {
+//     public Item item;
+//     public InventorySlot(Item _item) {
+//         {
+//             item = _item;
+//         }
+//     }
+// }
