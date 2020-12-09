cec969d0b3b921b43aff8105b11dd35e444d6fa1
Jacob Compton <jrcompto@ucsc.edu>
2020-12-08 17:22:30 -0800

    Fruit Tree Implemented

---
+++ Assets/_ProjectAppleFarm/Scripts/FruitScripts/FruitTree.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitTree : MonoBehaviour
+{
+    [SerializeField]
+    Fruit newFruit;
+    [SerializeField]
+    GameObject fruitSpawnLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void dropFruit(){
+        print("Dropping Fruit");
+        // Fruit newFruit = new Fruit();
+        // Instantiate(newFruit, new Vector3(569.9f,7.84f,-716.4f), Quaternion.identity);
+        Instantiate(newFruit, fruitSpawnLocation.transform.position, Quaternion.identity);
+    }
+}
