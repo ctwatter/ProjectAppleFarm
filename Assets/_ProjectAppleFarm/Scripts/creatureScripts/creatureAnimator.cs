cc1407f6256d1ff3ee0260aaca83e0dacf3fc700
Rookirk <hwu24110@gmail.com>
2020-12-16 14:06:59 -0800

    Began setting up Fragaria animations

---
+++ Assets/_ProjectAppleFarm/Scripts/creatureScripts/creatureAnimator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creatureAnimator : MonoBehaviour
+{
+    public GameObject model;
+    public Animator animator => model.GetComponent<Animator>();
+
+}
